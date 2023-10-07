5d778dc65430fc5a05477e48cda1cd8f5dd7b245
active <66892517+activ33@users.noreply.github.com>
2023-10-07 13:57:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters, enums
 from AnilistPython import Anilist
 anilist = Anilist()
 
-bot = Client("My First Project",api_id=24541704,
-             api_hash="7b1b63a5c5a2e53233a0e78727c068d3",
-             bot_token="6483695475:AAEUrXwwE-kMRhTZAL48o60u3j4nbbZsL3g")
+bot = Client("My First Project",api_id=2704,
+             api_hash="73a5c5a2e53233a0e73",
+             bot_token="6483695475:AAEUrXwwE-kMRhTZAL48o60u3")
 
 @bot.on_message(filters.command('start'))
 def start1(bot, message):
